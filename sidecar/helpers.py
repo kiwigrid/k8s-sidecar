64bbc8300f86a2926f48038e14aee3be037a63d5
holmesb <5072156+holmesb@users.noreply.github.com>
2020-06-30 21:36:16 +0100

    typo in helpers.py
    Should have been brace, not parentheses

--- sidecar/helpers.py
+++ sidecar/helpers.py
@@ -89,7 +89,7 @@ def request(url, method, payload=None):
     elif method == "POST":
         res = r.post("%s" % url, auth=auth, json=payload, timeout=timeout)
         print(f"{timestamp()} {method} request sent to {url}. "
-              f"Response: {res.status_code} {res.reason} (res.text}")
+              f"Response: {res.status_code} {res.reason} {res.text}")
     return res
 
 
