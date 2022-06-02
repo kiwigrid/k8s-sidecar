7e0401bd20d83e9a7ec85ffe2195215a2a9eac72
Jörg Eichhorn <joerg.eichhorn@kiwigrid.com>
2022-06-02 11:36:05 +0200

    chore: minor code style changes
    * let the process fail on misconfiguration
    * configure real root logger (so that library logs might get visible a well)

--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from typing import Optional
 
 from dateutil.tz import tzlocal, tzutc
 from logfmter import Logfmter
@@ -17,61 +18,57 @@ level = os.getenv("LOG_LEVEL", logging.INFO)
 fmt = os.getenv("LOG_FORMAT", 'JSON')
 tz = os.getenv("LOG_TZ", 'LOCAL')
 
-# Initialize logger
-root_logger = logging.getLogger("k8s-sidecar")
-log_handler = logging.StreamHandler()
-log_level = level.upper() if isinstance(level, str) else level
 log_tz = LogTimezones[tz.upper()] if LogTimezones.get(tz.upper()) else LogTimezones['LOCAL']
 
 
-# Base Formatter to enforce time format in ISO8601 with LOCAL or UTC Timezone
-class BaseFormatter(logging.Formatter):
-    def formatTime(self, record, timeFormat=None):
-        if timeFormat is not None:
-            return super(BaseFormatter, self).formatTime(record, timeFormat)
+class Iso8601Formatter:
+    """
+    A formatter mixin which always forces dates to be rendered in iso format.
+    Using `datefmt` parameter of logging.Formatter is insufficient because of missing fractional seconds.
+    """
+
+    def formatTime(self, record, datefmt: Optional[str] = ...):
+        """
+        Meant to override logging.Formatter.formatTime
+        """
         return datetime.fromtimestamp(record.created, log_tz).isoformat()
 
 
-# Define formatter using LogFmt format (time in ISO8601)
-class LogfmtFormatter(Logfmter, BaseFormatter):
-    def __init__(self, keys, mapping):
-        super(LogfmtFormatter, self).__init__(keys, mapping)
+class LogfmtFormatter(Iso8601Formatter, Logfmter):
+    """
+    A formatter combining logfmt style with iso dates
+    """
+    pass
 
 
-# Define formatter using Json format (time in ISO8601)
-class JsonFormatter(BaseFormatter, jsonlogger.JsonFormatter):
+class JsonFormatter(Iso8601Formatter, jsonlogger.JsonFormatter):
+    """
+    A formatter combining json logs with iso dates
+    """
+
     def add_fields(self, log_record, record, message_dict):
+        log_record['time'] = self.formatTime(record)
         super(JsonFormatter, self).add_fields(log_record, record, message_dict)
-        if not log_record.get('msg'):
-            log_record['msg'] = log_record['message']
-        if not log_record.get('time'):
-            log_record['time'] = self.formatTime(record)
-        if log_record.get('level'):
-            log_record['level'] = log_record['level'].upper()
-        else:
-            log_record['level'] = record.levelname
 
 
 # Supported Log Formatters
 LogFormatters = {
-    'JSON': (JsonFormatter('%(time)s %(level)s %(msg)s')),
+    'JSON': (JsonFormatter('%(levelname)s %(message)s',
+                           rename_fields={"message": "msg", "levelname": "level"})),
     'LOGFMT': (LogfmtFormatter(keys=["time", "level", "msg"],
                                mapping={"time": "asctime", "level": "levelname", "msg": "message"}))
 }
 
 log_fmt = LogFormatters[fmt.upper()] if LogFormatters.get(fmt.upper()) else LogFormatters['JSON']
 
-try:
-    log_handler.setFormatter(log_fmt)
-    root_logger.addHandler(log_handler)
-    root_logger.setLevel(log_level)
-except (ValueError, TypeError) as e:
-    root_logger.warning(f"Initializing default logger", exc_info=True)
-    root_logger.setLevel(logging.INFO)
-    log_handler.setFormatter(log_fmt)
-
+# Initialize/configure root logger
+root_logger = logging.getLogger()
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(log_fmt)
+root_logger.addHandler(log_handler)
+root_logger.setLevel(level.upper() if isinstance(level, str) else level)
 root_logger.addHandler(log_handler)
 
 
 def get_logger():
-    return root_logger
+    return logging.getLogger('k8s-sidecar')
